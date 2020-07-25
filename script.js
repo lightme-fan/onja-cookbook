aa9fcc3703f1cf45adf29768dc0af4be06660e16
Fanilo Andriamarohaja <fanilo.and@onja.org>
2020-07-25 09:45:36 +0100

    I have fixed the ingredient and step button. When I click the these buttons the new input will appear

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('It works');
+// Grabbing elements I need
+const form = document.querySelector('form');
+const inputs = form.querySelectorAll('input');
+const submitBtn = form.querySelectorAll('.sbumit-btn');
+
+const ingredientBtn = form.querySelector('.ingredients-btn');
+const stepBtn = form.querySelector('.step-btn');
+
+// Function to handle the ingredient button
+const handleIngredientBtn = (event) => {
+  event.preventDefault();
+  const newIngredient = `
+  <label for="steps"></label>
+  <input type="text" id="steps">
+  `
+  const ingredient = document.querySelector('.ingredient');
+  ingredient.insertAdjacentHTML('beforeend', newIngredient);
+}
+ingredientBtn.addEventListener('click', handleIngredientBtn);
+
+// A function to handle step button to add new process
+const handleStepBtn = (event) => {
+  event.preventDefault();
+  const newStep = `
+  <label for="steps"></label>
+  <input type="text" id="steps">
+  `
+  const step = document.querySelector('.step');
+  step.insertAdjacentHTML('beforeend', newStep);
+}
+stepBtn.addEventListener('click', handleStepBtn);
