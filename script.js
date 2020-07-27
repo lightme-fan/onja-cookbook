693707affa4e59e7cbb0fa3791a9a35aa1c609fe
Fanilo Andriamarohaja <fanilo.and@onja.org>
2020-07-27 10:37:48 +0100

    Updated onja coockbook

--- script.js
+++ script.js
@@ -2,28 +2,27 @@
 const form = document.querySelector('form');
 const inputs = form.querySelectorAll('input');
 const submitBtn = form.querySelectorAll('.sbumit-btn');
-
 const ingredientBtn = form.querySelector('.ingredients-btn');
 const stepBtn = form.querySelector('.step-btn');
 
 // Function to handle the ingredient button
 const handleIngredientBtn = (event) => {
-  event.preventDefault();
-  const newIngredient = `
-  <input type="text" id="ingredients" name="ingredient">
-  `
   const ingredient = document.querySelector('.ingredient');
+  const ingrendientNumber = ingredient.children.length + 1;
+  const newIngredient = `
+  <input type="text" id="ingredient${ingrendientNumber}" name="ingredient${ingrendientNumber}">
+  `
   ingredient.insertAdjacentHTML('beforeend', newIngredient);
 }
 ingredientBtn.addEventListener('click', handleIngredientBtn);
 
 // A function to handle step button to add new process
 const handleStepBtn = (event) => {
-  event.preventDefault();
-  const newStep = `
-  <input type="text" id="steps" name="step">
-  `
   const step = document.querySelector('.step');
+  const stepNum = step.children.length + 1;
+  const newStep = `
+  <input type="text" id="step${stepNum}" name="step${stepNum}">
+  `
   step.insertAdjacentHTML('beforeend', newStep);
 }
 stepBtn.addEventListener('click', handleStepBtn);
@@ -31,38 +30,37 @@ stepBtn.addEventListener('click', handleStepBtn);
 // Handling the submit button
 const handleSubmitBtn = (event) => {
   event.preventDefault();
-  const forms = event.target;
+  const forms = event.currentTarget;
   
   // Grabbing all inputs
-  const recipeName = forms.recipe.value;
-  const imageUrl = forms.picture.value;
-  const author = forms.author.value;
-  const level = forms.level.value;
-  const timing = forms.time.value;
-
+  // Distructure
+  const { recipeName, imageUrl, author, level, timing } = forms;
+    
+  // Looping
   const ingredient = document.querySelector('.ingredient');
+  const ingredients = [];
   for(let i = 0; i < ingredient.children.length; i++) {
-    const ingredientList = ingredient.children;
-    ingredientList[i] = form.ingredient.value;
+    ingredients.push(ingredient.children[i].children[0].value);
+    console.log(ingredients);
   }
 
   const step = document.querySelector('.step');
+  const steps = [];
   for (let i = 0; i < step.children.length, i++;) {
-    const stepList = step.children;
-    stepList[i] = form.step.value;
+    steps.push(step.children[i].children[0].value);
   }
 
 
   // This is the recipe array
   const recipe = [
     {
-      title: `${recipeName}`,
-      picture: `${imageUrl}`,
-      author: `${author}`,
-      difficulty: `${level}`,
-      timing: `${timing}`,
-      ingredients: `${ingredient}`,
-      steps: `${step}`
+      title: `${recipeName.value}`,
+      picture: `${imageUrl.value}`,
+      author: `${author.value}`,
+      difficulty: `${level.value}`,
+      timing: `${timing.value}`,
+      ingredients: `${ingredients}`,
+      steps: `${steps}`
     },
   ];
 
@@ -70,18 +68,3 @@ const handleSubmitBtn = (event) => {
 }
 
 form.addEventListener('submit', handleSubmitBtn);
-
-// function submitForms() {
-//   var forms = document.getElementsByTagName("form");
-//   var inputs;
-//   var tempResult = {};
-//   result = [];
-//   for (var i = 0; i < forms.length; i++) {
-//     inputs = forms[i].getElementsByTagName("input");
-//     tempResult = {};
-//     for (var j = 0; j < inputs.length; j++)
-//       tempResult[inputs[j].name] = inputs[j].value;
-//     result.push(tempResult);
-//   }
-//   console.log(result);
-// }
